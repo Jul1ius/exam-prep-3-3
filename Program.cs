eaa1adb84b82aaa1398dfecb010d1b351214a52d
Julii Abadjiev <117214264+Jul1ius@users.noreply.github.com>
2023-11-04 15:15:17 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {     
+        int[] numbers = Console.ReadLine()
+            .Split(' ')
+            .Select(int.Parse)
+            .ToArray();
+
+        int N = int.Parse(Console.ReadLine());
+        double sum = 0;
+        int count = 0;
+
+        for (int i = numbers.Length - 1; i >= 0 && count < N; i--)
+        {
+            sum += numbers[i];
+            count++;
+        }
+        double average = sum / count;
+
+        Console.WriteLine($"{average:F2}");
+    }
+}
